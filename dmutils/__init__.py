dfcb242be54b52bd7d75758a43974d23bd00734c
sarneaud <sarneaud@users.noreply.github.com>
2016-08-08 15:43:44 +1000

    Fixes for new shared utils

--- dmutils/__init__.py
+++ dmutils/__init__.py
@@ -1,6 +1,6 @@
 from . import logging, config, proxy_fix, formats, request_id
-from .flask_init import init_app, init_manager
+from .flask_init import init_app, init_frontend_app, init_manager
 
 import flask_featureflags
 
-__version__ = '24.2.0'
+__version__ = '24.4.1'
