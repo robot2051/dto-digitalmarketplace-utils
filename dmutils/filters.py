f229ec0163c792b9e719a58aabeb2e157a769e37
prakash <prakash@timilsina.com>
2016-09-13 15:14:16 +1000

    added timesince

--- dmutils/filters.py
+++ dmutils/filters.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 import re
 from markdown import markdown
 from flask import Markup
+from datetime import datetime
 
 
 def markdown_filter(text, *args, **kwargs):
@@ -45,3 +46,30 @@ def format_links(text):
         return formatted_text
     else:
         return text
+
+
+def timesince(dt, default="just now"):
+    """
+    Returns string representing "time since" e.g.
+    3 days ago, 5 hours ago etc.
+    """
+
+    now = datetime.utcnow()
+    diff = now - dt
+
+    periods = (
+        (diff.days / 365, "year", "years"),
+        (diff.days / 30, "month", "months"),
+        (diff.days / 7, "week", "weeks"),
+        (diff.days, "day", "days"),
+        (diff.seconds / 3600, "hour", "hours"),
+        (diff.seconds / 60, "minute", "minutes"),
+        (diff.seconds, "second", "seconds"),
+    )
+
+    for period, singular, plural in periods:
+
+        if period:
+            return "%d %s ago" % (period, singular if period == 1 else plural)
+
+    return default
